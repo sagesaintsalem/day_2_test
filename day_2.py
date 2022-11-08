6c742403d3486eb85ebebe3bef31150c2450515e
codeclan student <codeclanstudent@codeclans-MacBook-Pro-2.local>
2022-11-08 11:31:08 +0000

    Logic ops

--- day_2.py
+++ day_2.py
@@ -13,9 +13,11 @@ else:
 
 real = input("Am I real? (y or n): ")
 
+
 if real == "y":
     print("I am real!")
 elif real == "n":
     print("Oh dear God...")
 else:
     print("I did not understand that. Please try again.")
+        
---
+++ logical_ops.py
@@ -0,0 +1,10 @@
+me_hungry = False
+me_tired = False
+
+if me_hungry and me_tired == True:
+    print("Me cranky!")
+elif me_tired or me_hungry == True:
+    print("Me sad...")
+else:
+    print("Me happy!")
+
