d0d0db36e8f695b6b8012f1ea0f1a7e2e4efc1a4
codeclan student <codeclanstudent@codeclans-MacBook-Pro-2.local>
2022-11-08 11:47:13 +0000

    Chop n change

--- day_2.py
+++ day_2.py
@@ -8,16 +8,3 @@ elif is_it_raining == "i don't know":
     print("Go find out then!")
 else:
     print("What?")
-
-
-
-real = input("Am I real? (y or n): ")
-
-
-if real == "y":
-    print("I am real!")
-elif real == "n":
-    print("Oh dear God...")
-else:
-    print("I did not understand that. Please try again.")
-        
---
+++ reality.py
@@ -0,0 +1,10 @@
+real = input("Am I real? (y or n): ")
+
+
+if real == "y":
+    print("I am real!")
+elif real == "n":
+    print("Oh dear God...")
+else:
+    print("I did not understand that. Please try again.")
+        
