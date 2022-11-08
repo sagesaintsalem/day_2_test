1ca1db3382f7405cd6e83eeb0dd3907fd2145afa
codeclan student <codeclanstudent@codeclans-MacBook-Pro-2.local>
2022-11-08 15:38:54 +0000

    Hulk SMASH but prettier

--- dictionaries/dict_exercise.py
+++ dictionaries/dict_exercise.py
@@ -1,4 +1,10 @@
-avengers = {"Iron Man":{"name":"Tony Stark", "attacks":{"punch":10, "kick":100}},
- "Hulk":{"name":"Bruce Banner", "attacks":{"SMASH":1000, "roll":500}}}
+avengers = {"Iron Man":{
+    "name":"Tony Stark", 
+    "attacks":{
+        "punch":10, "kick":100}},
+ "Hulk":{
+    "name":"Bruce Banner", 
+    "attacks":{
+        "SMASH":1000, "roll":500}}}
 
 print(avengers["Hulk"]["attacks"]["SMASH"])
