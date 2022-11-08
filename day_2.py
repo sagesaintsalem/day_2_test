258c9b722102a3a11f6a8dec3b5b1ea041d1bc7c
codeclan student <codeclanstudent@codeclans-MacBook-Pro-2.local>
2022-11-08 10:59:34 +0000

    Conditionals practice

--- day_2.py
+++ day_2.py
@@ -1,1 +1,21 @@
-print("Get rekt")
+is_it_raining = input("Is it raining? ")
+
+if is_it_raining == "yes":
+    print("You should take the car")
+elif is_it_raining == "no":
+    print("On your bike!")
+elif is_it_raining == "i don't know":
+    print("Go find out then!")
+else:
+    print("What?")
+
+
+
+real = input("Am I real? (y or n): ")
+
+if real == "y":
+    print("I am real!")
+elif real == "n":
+    print("Oh dear God...")
+else:
+    print("I did not understand that. Please try again.")
