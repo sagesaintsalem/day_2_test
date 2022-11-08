cf876368041c0cf780d554c3cfd1440bfe210965
codeclan student <codeclanstudent@codeclans-MacBook-Pro-2.local>
2022-11-08 16:32:05 +0000

    Look at all these CHICKENS

---
+++ loops/loop_ex.py
@@ -0,0 +1,10 @@
+chickens = [
+  {"name": "Margaret", "age": 2, "eggs": 0},
+  {"name": "Hetty", "age": 1, "eggs": 2},
+  {"name": "Henrietta", "age": 3, "eggs": 1},
+  {"name": "Audrey", "age": 2, "eggs": 0},
+  {"name": "Mabel", "age": 5, "eggs": 1},
+]
+
+for chicken in chickens:
+    print(f'{chicken["name"]} is {chicken["age"]}')
