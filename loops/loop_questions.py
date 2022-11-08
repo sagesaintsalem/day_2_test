3f0c22cc5b135b57b3a76279d86772d5d2a55dea
codeclan student <codeclanstudent@codeclans-MacBook-Pro-2.local>
2022-11-08 16:19:52 +0000

    Loopdeloop

---
+++ loops/loop_questions.py
@@ -0,0 +1,8 @@
+my_number = 5
+
+value = int(input("What number am I thinking of? "))
+
+while (value != my_number):
+    value = int(input("Nope! Try again! "))
+
+print("Correct!")
---
+++ loops/loops.py
@@ -0,0 +1,30 @@
+# #While loop
+
+# counter = 1
+# my_num = 6
+
+# while counter < my_num:
+#     print(counter)
+#     counter += 1
+
+# nutz = 2
+# print("  ")
+# print("  ")
+# print("  ")
+
+# while counter > nutz:
+#     print(counter)
+#     counter -= 1
+# print("Counter is at " + str(counter) + " and I have " + str(nutz) + " Nutz.")
+
+# while(True):
+#     line = input("Type something...")
+#     if(line.lower() == "stop"):
+#         break
+#     print(f"You typed {line}...")
+# print(f"You typed {line} and escaped the loop!")
+
+numbers = [1, 2, 3, 4, 5]
+
+for i in numbers:
+    print(i)
