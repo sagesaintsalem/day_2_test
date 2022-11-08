b7ae2c3d0e558a25a3b65eca9a50fa1f5b76b1bd
codeclan student <codeclanstudent@codeclans-MacBook-Pro-2.local>
2022-11-08 13:45:51 +0000

    Changed to fit exercise

--- lists/lists_exercise.py
+++ lists/lists_exercise.py
@@ -1,11 +1,16 @@
-task_list = ["make dinner", "shower", "make necklace"]
-
+# 1. Create an empty list called `task_list`
+task_list = []
+# 2. Add a few `str` elements, representing some everyday tasks e.g. 'Make Dinner'
+task_list.append("make dinner") 
+task_list.append("shower") 
+task_list.append("make necklace")
+# 3. Print out `task_list`
 print(task_list)
-
+# 4. Remove the last task
 print(task_list.pop())
-
+# 5. Print out `task_list`
 print(task_list)
-
+# 6. Print out the number of elements in `task_list`
 print(len(task_list))
 
 
